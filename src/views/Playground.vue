60bcb62573d28e17a8e61e0e58843889f03efe36
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-02-18 14:46:43 +0300

    add a playgroun view and call a dummy api, ofc it works. Also, added axios

---
+++ src/views/Playground.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="playground">{{ info }}</div>
+</template>
+
+<script>
+const axios = require("axios").default;
+
+// axios.<method> will now provide autocomplete and parameter typings
+export default {
+  data() {
+    return {
+      info: null,
+    };
+  },
+  beforeCreate() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => (this.info = response));
+  },
+};
+</script>
+
+<style></style>
