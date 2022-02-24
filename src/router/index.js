8e5330da192da8af19af0f6b3f8c9c0282da335e
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-02-24 16:21:23 +0300

    anasayfa seems to be working kinda

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,15 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/Kayit.vue"),
   },
   {
+    path: "/anasayfa",
+    name: "Anasayfa",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Anasayfa.vue"),
+  },
+  {
     path: "/playground",
     name: "Playground",
     // route level code-splitting
