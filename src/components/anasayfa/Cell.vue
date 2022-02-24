396631e157ad910ace8beff3eb803507715fb801
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-02-24 17:35:39 +0300

    fallback

--- src/components/anasayfa/Cell.vue
+++ src/components/anasayfa/Cell.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="cellComponent row">
+  <div class="cellComponent">
     <div class="logoBox">
       <img src="../../assets/anasayfa/gorusme.svg" alt="" class="logo" />
     </div>
@@ -10,22 +10,29 @@
       <div class="row colTitle redTitle">Görüşme Bekleniyor</div>
       <div class="row colPara">20 Ocak Pazartesi - 12:30</div>
     </div>
-    <div
-      class="col box 1stBox d-flex flex-column justify-content-center align-items-start"
-    >
-      <div class="row colTitle redTitle">Görüşme Bekleniyor</div>
-      <div class="row colPara">20 Ocak Pazartesi - 12:30</div>
-    </div>
-    <div
-      class="col box 1stBox d-flex flex-column justify-content-center align-items-start"
-    >
-      <div class="row colTitle redTitle">Görüşme Bekleniyor</div>
-      <div class="row colPara">20 Ocak Pazartesi - 12:30</div>
-    </div>
 
     <!-- second box -->
-
+    <div
+      class="col box 1stBox d-flex flex-row justify-content-start align-items-center"
+    >
+      <img src="../../assets/anasayfa/doktor.svg" alt="" class="doktor" />
+      <div>
+        <div class="row colTitle redTitle">Görüşme Bekleniyor</div>
+        <div class="row colPara">20 Ocak Pazartesi - 12:30</div>
+      </div>
+    </div>
     <!-- third box -->
+    <div
+      class="col box 1stBox d-flex flex-row justify-content-start align-items-center"
+    >
+      <div class="greyBox">
+        <img src="../../assets/anasayfa/hospital.svg" alt="" class="greyLogo" />
+      </div>
+      <div>
+        <div class="row colTitle redTitle">Görüşme Bekleniyor</div>
+        <div class="row colPara">20 Ocak Pazartesi - 12:30</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -43,14 +50,13 @@ export default {};
   border-radius: 10px;
   margin-bottom: 20px;
   margin-top: 20px;
-  height: auto;
+  height: 110px;
 }
-.cell {
-  width: auto;
+.row {
   height: 100%;
 }
 .col {
-  height: 100%;
+  height: 100% !important;
 }
 .box {
   height: 100%;
@@ -85,6 +91,11 @@ export default {};
   /* Primary */
   color: #3c4e69;
 }
+.doktor {
+  width: 50px;
+  height: 50px;
+  margin-right: 1rem;
+}
 .logoBox {
   width: 40px;
   height: 100px;
@@ -107,10 +118,28 @@ export default {};
   border-radius: 0px;
   margin: 0;
 }
+.greyBox {
+  width: 50px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #f2f8fb;
+  border-radius: 5px;
+  margin-right: 15px;
+  /* this padding makes sure that the logo fits well */
+  padding: 10px;
+}
+.greyLogo {
+  width: 22px;
+  height: 22px;
+  margin-right: 1rem;
+}
 
 @media (max-width: 1200px) {
   .cellComponent {
     flex-direction: column;
+    height: 100%;
   }
   .logoBox {
     width: 100%;
