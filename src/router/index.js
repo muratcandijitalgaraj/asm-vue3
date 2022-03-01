621e0cb77e361bc40eeebb2e7bd555c1579948eb
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-03-01 14:11:07 +0300

    start randevularim bos component

--- src/router/index.js
+++ src/router/index.js
@@ -47,6 +47,15 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/RandevuDetay.vue"),
   },
   {
+    path: "/randevularim-bos",
+    name: "RandevularimBos",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Randevularim-bos.vue"),
+  },
+  {
     path: "/playground",
     name: "Playground",
     // route level code-splitting
